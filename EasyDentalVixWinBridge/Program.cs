c2d75bc61ed2feaafbfc50b8f70d92017a890118
Brian Mak <eggrollbot@gmail.com>
2020-12-23 18:16:25 -0800

    Made it so program can only have one instance open

--- EasyDentalVixWinBridge/Program.cs
+++ EasyDentalVixWinBridge/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace EasyDentalVixWinBridge
@@ -14,9 +15,19 @@ namespace EasyDentalVixWinBridge
 		[STAThread]
 		internal static void Main( )
 		{
-			Application.EnableVisualStyles( );
-			Application.SetCompatibleTextRenderingDefault( false );
-			Application.Run( new App( ) );
+			using ( Mutex appMutex = new Mutex( false, "Global\\{bce94a11-cb8f-41b5-a0b4-3cd8e10628cd}" ) )
+			{
+				if ( !appMutex.WaitOne( 0, false ) )
+				{
+					MessageBox.Show( "Application is already running.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error );
+
+					return;
+				}
+
+				Application.EnableVisualStyles( );
+				Application.SetCompatibleTextRenderingDefault( false );
+				Application.Run( new App( ) );
+			}
 		}
 	}
 }
